6edf78302e5a0556bba284214da7504783af90c5
kherringshaw <kherringshaw@luc.edu>
2016-04-11 09:06:51 -0500

    first commit

--- src/test1/core.clj
+++ src/test1/core.clj
@@ -8,112 +8,60 @@
 
 
 (def mysql-db {:subprotocol "mysql"
-               :subname "//127.0.0.1:3306/restaurant"
+               :subname "//127.0.0.1:3306/diagnostics"
                :user "root"
                :password "fpl350clark"})
 
 
-
-(j/insert! mysql-db :restaurantattr
-           {:restaurantId 1 :restaurantName "Mastros" :location "Lincoln Park" :price 15.0 :cuisine "Steakhouse" :allergyAlerts "peanuts" :atmosphere "fun" :rating 3}
-           {:restaurantId 2 :restaurantName "Wild Ginger" :location "Downtown" :price 20.0 :cuisine "Asian" :allergyAlerts "peanuts, shellfish" :atmosphere "lively" :rating 3}
-           {:restaurantId 3 :restaurantName "Bombay Cafe" :location "Downtown" :price 25.0 :cuisine "Asian" :allergyAlerts "none" :atmosphere "casual" :rating 4}
-           {:restaurantId 4 :restaurantName "Sammy's" :location "Lincoln Park" :price 35.0 :cuisine "Steakhouse" :allergyAlerts "none" :atmosphere "fun" :rating 3}
-           {:restaurantId 5 :restaurantName "Boneyard" :location "Downtown" :price 25.0 :cuisine "BBQ" :allergyAlerts "none" :atmosphere "lively" :rating 2}
-           {:restaurantId 6 :restaurantName "Boss Hog" :location "Downtown" :price 10.0 :cuisine "BBQ" :allergyAlerts "none" :atmosphere "casual" :rating 3}
-           {:restaurantId 7 :restaurantName "Appletons" :location "Lincoln Park" :price 15.0 :cuisine "Steakhouse" :allergyAlerts "none" :atmosphere "fun" :rating 4}
-           {:restaurantId 8 :restaurantName "Mandarian" :location "Downtown" :price 25.0 :cuisine "Asian" :allergyAlerts "peanuts" :atmosphere "lively" :rating 4}
-           {:restaurantId 9 :restaurantName "Tex Mex" :location "Lincoln Park" :price 23.0 :cuisine "BBQ" :allergyAlerts "none" :atmosphere "casual" :rating 3}
-           {:restaurantId 10 :restaurantName "Cross Roads" :location "Downtown" :price 8.0 :cuisine "Steakhouse" :allergyAlerts "shellfish" :atmosphere "lively" :rating 3}
+;;most common programming errors defined and suggested fixes
+(j/insert! mysql-db :issues
+           {:issue "illegal start of expression" :def "This error occurs when an expression is expected but not found." :fix1 "An extra right parenthesis after the condition of an if-statement" :fix2 "Forgetting the right-hand side of an assignment statement." }
+           {:issue  "not a statement" :def "This error occurs when a syntactically correct statement does not appear where it should." :fix1 "Writing an assignment statement without the assignment operator"  :fix2 "Misspelling else"}
+           {:issue "cannot find symbol" :def "All identifiers in JAVA must be declared before being used" :fix1 "An identifier can only be declared once in the same scope" :fix2 "Misspelling else"}
+           {:issue "variable . . . might not have been initialized" :def "An instance variable declared in a class has a default initial value. However, a local variable declared within a method does not." :fix1 "The variable must be initialized on every path from declaration" :fix2 ""}
+           {:issue "invalid method declaration; return type required" :def "Every method except constructors must have a return type or void specified." :fix1 "The error frequently occurs because it is easy to misspell the name of a constructor" :fix2 " The error can occur if you write a statement after a return statement"}
            )
 
 
 
-(defn rule1[location price]
-    "Location and price are restricted, so query
-    database on given attributes."
-  (println(str location))
-  (println (str price))
-  
+
+
+(defn rule1[issue]
+  (println(str issue))
+   "User received an error and they type in one word
+    lookup, so query database on given word."
   (j/query mysql-db
-  ["select restaurantName, location, price from restaurantattr where location = ? AND price <= ?" location price]
+  ["select def, fix1, fix2 from issues where issue LIKE ? "  issue]
   :row-fn println)
   (println)
 
 )
 
-(defn rule2[location]
-    "Location is restricted, so query
-    database on given attribute."
-  (println(str location))
-  
-  (j/query mysql-db
-  ["select restaurantName, location from restaurantattr where location = ? " location ]
-  :row-fn println)
-  (println)
 
-)
 
-(defn rule3[price]
-  (println (str price))
-    "Price is restricted, so query
-    database on given attribute."
-  (j/query mysql-db
-  ["select restaurantName, price  from restaurantattr where price <= ?"  price]
-  :row-fn println)
-  (println)
-
-)
-
-(defn rule4[cuisine]
-  (println(str cuisine))
-   "Cuisine is restricted, so query
-    database on given attribute."
-  
-  (j/query mysql-db
-  ["select restaurantName, cuisine from restaurantattr where cuisine = ?" cuisine]
-  :row-fn println)
-  (println)
-
-)
-
-(defn rule5[cuisine rating price]
-  (println(str cuisine))
-  (println(str rating))
-  (println (str price))
-   "Cuisine, rating and price is restricted, so query
-    database on given attributes."
-  
-  (j/query mysql-db
-  ["select restaurantName, cuisine, rating, price from restaurantattr where cuisine = ? AND rating >= ? AND price <= ?" cuisine rating price]
-  :row-fn println)
-  (println)
-
-)
-
-(defn rule6[cuisine allergyAlerts]
-  (println(str cuisine))
-  (println (str allergyAlerts))
-   "Cuisine and does not contain allergyAlerts is
-    restricted, so query database on given attributes."
-  (j/query mysql-db
-  ["select restaurantName, cuisine, allergyAlerts from restaurantattr where cuisine = ? AND allergyAlerts != ?" cuisine allergyAlerts]
-  :row-fn println)
-  (println)
-
-)
-
-(defn rule7[startswith]
+(defn rule2[startswith]
    "Client trying to figure out a name, so query
     database on given attribute."
   (println(str startswith))
   
   (j/query mysql-db
-  ["select restaurantName from restaurantattr where restaurantName LIKE ? " startswith]
+  ["select def, fix1, fix2 from issues where issue LIKE ? " startswith]
   :row-fn println)
   (println)
 
 )
+
+(defn rule3[fix]
+  (println(str fix))
+   "User knows the fix and they type in one word
+    lookup, so query database on given word."
+  (j/query mysql-db
+  ["select def, fix1, fix2 from issues where fix LIKE ? "  fix]
+  :row-fn println)
+  (println)
+
+)
+
 
 
 
@@ -121,12 +69,9 @@
   "I can say 'Hello World'."
   (println "Hello, World!")
   (println)
-  (rule1 "Downtown" 15.0)
-  (rule2 "Lincoln Park")
-  (rule3 20.0)
-  (rule4 "Steakhouse")
-  (rule5 "Steakhouse" 3 45)
-  (rule6 "Steakhouse" "shellfish")
-  (rule7 "bo%")
+
+  (rule1 "illegal")
+  (rule2 "can%")
+  (rule3 "error")
 
   )
